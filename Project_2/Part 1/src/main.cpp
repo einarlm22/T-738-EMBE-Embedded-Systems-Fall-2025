82f31164d4e1de5f52cd0a9f5f768da5ebb360b3
Fannar <fannars22@ru.is>
2025-09-17 23:07:29 +0000

    Rpm implemented

--- Project_2/Part 1/src/main.cpp
+++ Project_2/Part 1/src/main.cpp
@@ -2,9 +2,16 @@
 #include "encoder.h"
 #include "digital_out.h"
 
-#define PRINT_MS 10     
+#define PRINT_MS 100     
+
+#define CPR 1400            
+#define RPM_WIN_MS 100
 
 Digital_out led(5);
+
+long last_pos = 0;
+unsigned long last_rpm_ms = 0;
+long rpm_val = 0;
 
 int main() {
   init();
@@ -15,11 +22,23 @@ int main() {
   unsigned long last = 0;
   while (1) {
     unsigned long now = millis();
+
+    if (now - last_rpm_ms >= RPM_WIN_MS) {
+      long pos = encoder_position();              
+      long delta = pos - last_pos;                
+      last_pos = pos;
+      last_rpm_ms += RPM_WIN_MS;                  
+
+      // RPM = delta * 600 / CPR  (delta is for 0.1 s)
+      if (CPR > 0) rpm_val = (delta * 600L) / CPR;
+      else         rpm_val = 0;
+    }
+
     if (now - last >= PRINT_MS) {
       last = now;
-      Serial.println(encoder_position()); 
+      Serial.print(encoder_position());
+      Serial.print(", rpm=");
+      Serial.println(rpm_val);
     }
-    led.set_hi();
-    led.set_lo(); 
   }
 }
